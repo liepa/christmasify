66fe6dfd0dc120ace1ae526e2ac348389bb11f99
Ernests Kecko <ernests.kecko@tieto.com>
2014-12-12 18:09:03 +0200

    Cleanup and replacing of minified js files

--- app/assets/javascripts/christmasify.js
+++ app/assets/javascripts/christmasify.js
@@ -1,6 +1,5 @@
 //= require three.js
-// require three-r54.min.js
-//= require dat.gui.min.js
+//= require dat.gui.js
 //= require detector.js
 //= require canvas_render.js
 //= require projector.js
